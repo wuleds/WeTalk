02372f6a9abb08e2bb29a68d413068fde1c6043c
吴乐 <wl1030ds@gmail.com>
2023-10-31 21:56:56 +0800

    美化

--- src/main/java/hjnu/wule/wetalk/controller/UserController.java
+++ src/main/java/hjnu/wule/wetalk/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.Objects;
 
 
